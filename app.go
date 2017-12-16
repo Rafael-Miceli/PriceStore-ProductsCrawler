94336b139da64b63d593bc349ab3845955b28813
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-12-16 16:01:09 -0200

    Encontrando preço em pagina web de teste

--- app.go
+++ app.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Initializing Products Crawler")
+
+	b, err := ioutil.ReadFile("test-page.html") // just pass the file name
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	str := string(b) // convert content to a 'string'
+
+	if strings.Contains(str, "R$") {
+		fmt.Printf("Found product price ")
+	} else {
+		fmt.Printf("NOT Found product price ")
+	}
 }
